ecbba99667ff67d4c95a2273c1713418d3b00256
hallo-netizen <hallo@oliver-beil.de>
2026-09-12 20:20:00 +0200

    Keep glossary filter routes ahead of legacy redirects

--- protocol/PROJECT_MEMORY/PROJEKTE/PFERDE_ATELIER/GLOSSAR/candidate-0.2.3/includes/class-uge-policy.php
+++ protocol/PROJECT_MEMORY/PROJEKTE/PFERDE_ATELIER/GLOSSAR/candidate-0.2.3/includes/class-uge-policy.php
@@ -3,7 +3,14 @@ if (!defined('ABSPATH')) { exit; }
 
 final class UGE_Policy {
     public static function init(): void {
+        add_action('template_redirect', [__CLASS__, 'protect_group_route'], 0);
         add_filter('the_content', [__CLASS__, 'append_primary_category_link'], 30);
+    }
+
+    public static function protect_group_route(): void {
+        if (is_tax(UGE_Core::TAXONOMY)) {
+            remove_action('template_redirect', [UGE_Core::class, 'redirect_legacy_term_url'], 1);
+        }
     }
 
     public static function append_primary_category_link(string $content): string {
