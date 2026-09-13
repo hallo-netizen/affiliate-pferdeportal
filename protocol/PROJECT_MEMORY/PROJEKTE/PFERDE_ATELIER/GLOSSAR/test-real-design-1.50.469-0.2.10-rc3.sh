887ca32cd6614a73a683beb83d624374baac7499
hallo-netizen <hallo@oliver-beil.de>
2026-09-13 15:42:33 +0200

    Inspect exact design page-type decision path

--- protocol/PROJECT_MEMORY/PROJEKTE/PFERDE_ATELIER/GLOSSAR/test-real-design-1.50.469-0.2.10-rc3.sh
+++ protocol/PROJECT_MEMORY/PROJEKTE/PFERDE_ATELIER/GLOSSAR/test-real-design-1.50.469-0.2.10-rc3.sh
@@ -17,8 +17,8 @@ assert needle in s
 probe=r'''test "$(docker exec wp wp plugin get affiliate-portal-template-kit/pferde-template-kit.php --field=version --allow-root)" = 1.50.469
 HEALTH_ID=$(docker exec wp wp post list --allow-root --post_type=page --name=gesundheit --field=ID)
 echo "REAL_DESIGN_HEALTH_ID=$HEALTH_ID"
-docker exec wp wp eval --allow-root "\$id=$HEALTH_ID; echo 'REAL_DESIGN_AFFILIATE_PAGE_TYPE=' . (method_exists('Pferde_Template_Kit','affiliate_page_type') ? Pferde_Template_Kit::affiliate_page_type(\$id) : 'NO_METHOD') . PHP_EOL; echo 'UGE_PRIMARY_TARGET='; var_export(UGE_Core::primary_category_target(\$id)); echo PHP_EOL; echo 'HEALTH_META='; var_export(get_post_meta(\$id)); echo PHP_EOL;"
-docker exec wp sh -c "grep -n -A100 -B20 'function affiliate_page_type' /var/www/html/wp-content/plugins/affiliate-portal-template-kit/pferde-template-kit.php || true"
+docker exec wp wp eval --allow-root "\$id=$HEALTH_ID; echo 'REAL_DESIGN_AFFILIATE_PAGE_TYPE=' . Pferde_Template_Kit::affiliate_page_type(\$id) . PHP_EOL; echo 'HEALTH_META='; var_export(get_post_meta(\$id)); echo PHP_EOL;"
+docker exec wp sh -c "grep -n -A180 -B20 'function current_page_template_type' /var/www/html/wp-content/plugins/affiliate-portal-template-kit/pferde-template-kit.php || true"
 '''
 s=s.replace(needle,probe,1)
 p.write_text(s)
