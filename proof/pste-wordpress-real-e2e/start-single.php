7757aef5def5aa51a4e62f807a16f8eb0734f340
hallo-netizen <hallo@oliver-beil.de>
2026-09-21 09:41:36 +0200

    PSTE real E2E persist single run id

--- proof/pste-wordpress-real-e2e/start-single.php
+++ proof/pste-wordpress-real-e2e/start-single.php
@@ -3,5 +3,6 @@ $termId=(int)get_option('pste_e2e_heutaschen_term_id',0);
 if($termId<=0)throw new RuntimeException('TERM_ID_MISSING');
 $settings=PSTE_Plugin::settings();
 $job=PSTE_Research_Job::start($termId,'Heutaschen',$settings);
+update_option('pste_e2e_single_job_uuid',(string)$job['job_uuid'],false);
 $driver=PSTE_Research_Driver::kick(0,'E2E_MANUAL_SINGLE');
 echo wp_json_encode(['job'=>$job,'driver'=>$driver],JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES)."\n";
